07b4a749d7904898fb62842af0a9ba5a46a38d0c
kimpmr <kimberllypmr@hotmail.com>
2024-05-22 11:05:24 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {
+  stickyFunction();
+};
+
+let navbar = document.getElementById("navbar");
+let sticky = navbar.offsetTop;
+
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
